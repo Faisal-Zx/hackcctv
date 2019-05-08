8f8427e7c4bcc789312f96dbf6dbf49cef6cf4cd
MrUncle <50101968+MrTamfanX@users.noreply.github.com>
2019-05-08 15:58:21 +0700

    Update fr.py

--- LIST/fr.py
+++ LIST/fr.py
@@ -27,12 +27,12 @@ def france():
     print("{}        ____ ").format(r)
     print("   _[]_/____\__n_ ")
     print("  |_____.--.__()_|")
-    print("  |I   //# \\\    |")
-    print("{}  |P   \\\__//    | ").format(w)
-    print("  |CS   '--'     | ")
+    print("  |TAM //# \\\  FAN|")
+    print("{}  |TAM \\\__// FAN| ").format(w)
+    print("  |TAM  '--'  FAN| ")
     print("{}  '--------------'----------{}------------------.  ").format(r,w)
-    print("{}  | {}Author  : {}HVmbl3 {}     | {}INDO{}N{}{}ESIA         | ").format(r,w,r,w,r,ir,reset,w)
-    print("{}  | {}Youtube : {}Shodiq 2701 {}| {}+62-813-6487-3762 {}|").format(r,w,w,w,lgray,w)
+    print("{}  | {}Author  : {}MrTamfanX {} | {}INDO{}N{}{}ESIA         | ").format(r,w,r,w,r,ir,reset,w)
+    print("{}  | {}Youtube : {}MrUncle    {}| {}+62-857-7951-5200 {}|").format(r,w,w,w,lgray,w)
     print("{}  '------------------------------------{}-------'  ").format(r,w)
     print("{}       [ {}List page : {} {}]").format(r,w,rfindpage,r)
     run()
@@ -51,4 +51,4 @@ def run():
              count += 1
     except:
         print ""
-        print r+"Makasi udh pake tools kami"+w
+        print r+"Terimakasih Buat Lu Semua Ngentod.."+w
